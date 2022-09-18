9d3813a7c9fa6cdc26b1f0b3180039524687818b
Nazgul-Willy-o-brabo <willyc.campos71271@gmail.com>
2022-09-18 13:45:01 -0300

    rot3

--- codigo/roteiro3/Program.cs
+++ codigo/roteiro3/Program.cs
@@ -1,2 +1,18 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+Console.WriteLine("selecione a atividade");
+int op = int.Parse(Console.ReadLine());
+switch (op)
+{
+    case 1:
+        atv01();
+        break;
+          case 2:
+            atv02();
+        break;
+        case 3:
+        atv03();
+        break;
+        case 4:
+        atv04();
+        break;
+}
