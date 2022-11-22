7f7a88ee06deae2809549f0be42cbe6be4d9df9d
Nazgul-Willy-o-brabo <willyc.campos71271@gmail.com>
2022-11-22 17:50:48 -0300

    roteiro10

---
+++ codigo/roteiro10/dinamico/Dados.cs
@@ -0,0 +1,16 @@
+﻿namespace dinamico
+{
+    internal class Dados
+    {
+        public class Funcionario
+        {
+            public string Nome;
+            public int Id;
+        }
+        public class Elemento
+        {
+            public Funcionario funcionario = new Funcionario();
+            public Elemento proximo = null;
+        }
+    }
+}
