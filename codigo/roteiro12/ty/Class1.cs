bf876ac153e1e15e89237aad45557462f147b3a7
Willy-Campos-Costa---Brabo <101759293+Nazgul-Willy-o-brabo@users.noreply.github.com>
2022-12-15 17:39:06 -0300

    Update Class1.cs

--- codigo/roteiro12/ty/Class1.cs
+++ codigo/roteiro12/ty/Class1.cs
@@ -6,7 +6,38 @@ using System.Threading.Tasks;
 
 namespace ty
 {
-    internal class Class1
+    class Arvore
     {
+        public void inserir(int valor, Nodo nodo)
+        {
+            if (valor > nodo.valorNodo)
+            {
+                if (nodo.fDireito == null)
+                {
+                    Nodo filho = new Nodo();
+                    filho.pai = nodo;
+                    filho.valorNodo = valor;
+                    nodo.fDireito = filho;
+                }
+                else
+                {
+                    inserir(valor, nodo.fDireito);
+                }
+            }
+            else
+            {
+                if (nodo.fEquerdo == null)
+                {
+                    Nodo filho = new Nodo();
+                    filho.pai = nodo;
+                    filho.valorNodo = valor;
+                    nodo.fEquerdo = filho;
+                }
+                else
+                {
+                    inserir(valor, nodo.fEquerdo);
+                }
+            }
+        }
     }
 }
