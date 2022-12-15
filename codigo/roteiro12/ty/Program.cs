a5c0345a0a24ba263bcef6e767d6856356af75d2
Nazgul-Willy-o-brabo <willyc.campos71271@gmail.com>
2022-12-15 17:09:05 -0300

    rot12

---
+++ codigo/roteiro12/ty/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ty
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Arvore arvore = new Arvore();
+            Nodo nodoRaiz = new Nodo();
+            nodoRaiz.valorNodo = 20;
+
+            arvore.inserir(10, nodoRaiz);
+            arvore.inserir(30, nodoRaiz);
+            arvore.inserir(15, nodoRaiz);
+        }
+    }
+}
