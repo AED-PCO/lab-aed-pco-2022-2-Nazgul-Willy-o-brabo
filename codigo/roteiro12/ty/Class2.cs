1bf735488c1ec98c7ce91e4f57d347874d89959e
Willy-Campos-Costa---Brabo <101759293+Nazgul-Willy-o-brabo@users.noreply.github.com>
2022-12-15 17:39:36 -0300

    Update Class2.cs

--- codigo/roteiro12/ty/Class2.cs
+++ codigo/roteiro12/ty/Class2.cs
@@ -6,7 +6,11 @@ using System.Threading.Tasks;
 
 namespace ty
 {
-    internal class Class2
+    class Nodo
     {
+        public Nodo pai = null;
+        public Nodo fDireito = null;
+        public Nodo fEquerdo = null;
+        public int valorNodo;
     }
 }
